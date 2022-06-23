7f3da670e28dbf42fef4ce668361705b1b3b4f45
Florian Scherf <mail@florianscherf.de>
2022-06-23 13:47:16 +0200

    release 0.1
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- simple_plugin_manager/__init__.py
+++ simple_plugin_manager/__init__.py
@@ -1,5 +1,5 @@
 from .plugin_manager import PluginManager  # NOQA
 from .loading import Loader, load  # NOQA
 
-VERSION = (0, 0)
+VERSION = (0, 1)
 VERSION_STRING = '.'.join(str(i) for i in VERSION)
