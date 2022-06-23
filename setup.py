219ae557021325708b959a2e5d501eee0ffe4a2f
Florian Scherf <mail@florianscherf.de>
2022-06-23 22:15:27 +0200

    release 0.1.1
    Signed-off-by: Florian Scherf <mail@florianscherf.de>

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 setup(
     include_package_data=True,
     name='simple-plugin-manager',
-    version='0.1',
+    version='0.1.1',
     author='Florian Scherf',
     url='https://github.com/fscherf/simple-plugin-manager',
     author_email='mail@florianscherf.de',
